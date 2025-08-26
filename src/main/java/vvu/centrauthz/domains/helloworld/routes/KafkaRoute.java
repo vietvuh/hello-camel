ff5de2192a49cf8bf10a70f768dac0244924fffc
Vu Hoang Viet <viet.vuhoang@outlook.com.vn>
2025-08-26 09:43:38 +0700

    Kafka integration

---
+++ src/main/java/vvu/centrauthz/domains/helloworld/routes/KafkaRoute.java
@@ -0,0 +1,30 @@
+package vvu.centrauthz.domains.helloworld.routes;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.builder.RouteBuilder;
+
+@ApplicationScoped
+@Slf4j
+public class KafkaRoute extends RouteBuilder  {
+    /**
+     * <b>Called on initialization to build the routes using the fluent builder syntax.</b>
+     * <p/>
+     * This is a central method for RouteBuilder implementations to implement the routes using the Java fluent builder
+     * syntax.
+     *
+     * @throws Exception can be thrown during configuration
+     */
+    @Override
+    public void configure() throws Exception {
+        from("direct:produce-cdc-events")
+            .routeId("produce-cdc-events-route")
+            .log("Producing CDC event to Kafka: ${body}")
+            .marshal().json()
+            .to("kafka:topic.ec.cdc.events.applications");
+
+        from("kafka:topic.test?groupId=test-consumer-group")
+            .routeId("consume-cdc-users-route")
+            .log("Consumed CDC user event from Kafka: ${body}");
+    }
+}
